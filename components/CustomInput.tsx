6514e9e3b9c00094330eea52072dd402e529e9a9
Misaponce <poncemisael@gmail.com>
2023-08-17 12:15:46 +0100

    Update CustomInput component to display the placesholder instead of the value

--- components/CustomInput.tsx
+++ components/CustomInput.tsx
@@ -1,12 +1,15 @@
 import { CustomInputProps } from '@/types'
 import React from 'react'
 
-const CustomInput = ({ placeHolder, type, inputId, onChange}:CustomInputProps) => {
+const CustomInput = ({ placeHolder, type, inputId, onChange, value}:CustomInputProps) => {
   return (
     <input 
     type={type}
     placeholder={placeHolder}
     id={inputId}
+    // With this ternary condition, I render an empty string if the value is 0
+    // otherwise it won't show the placeholder 'Custom' on the input
+    value={value === 0 ? '' : value}
     onChange={onChange} 
     className='
       form-input 
